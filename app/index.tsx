a8bcce975c98c37b4aad678d3815ac086fbc8196
Alef Basilio <alefsilva0505@gmail.com>
2025-06-05 12:41:22 -0300

    new interface

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
-import { FlatList, Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
-import { Checkbox } from "expo-checkbox";
-import { useEffect, useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from 'react';
+import { FlatList, Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Checkbox } from 'expo-checkbox';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type TaskType = {
   id: number;
@@ -14,12 +12,12 @@ type TaskType = {
 
 export default function HomeScreen() {
   const [tasks, setTasks] = useState<TaskType[]>([]);
-  const [taskText, settaskText] = useState<string>("");
+  const [taskText, settaskText] = useState<string>('');
 
   useEffect(() => {
     const getTasks = async () => {
       try {
-        const tasks = await AsyncStorage.getItem("my-task");
+        const tasks = await AsyncStorage.getItem('my-task');
         if (tasks !== null) {
           setTasks(JSON.parse(tasks));
         }
@@ -30,17 +28,17 @@ export default function HomeScreen() {
     getTasks();
   }, []);
 
-  const addtask = async () => {
+  const addTask = async () => {
     try {
-      const newtask = {
+      const newTask = {
         id: Math.random(),
         title: taskText,
         isDone: false,
       };
-      tasks.push(newtask);
+      tasks.push(newTask);
       setTasks(tasks);
-      await AsyncStorage.setItem("my-task", JSON.stringify(tasks));
-      settaskText("");
+      await AsyncStorage.setItem('my-task', JSON.stringify(tasks));
+      settaskText('');
       Keyboard.dismiss();
     } catch (error) {
       console.log(error);
@@ -50,7 +48,7 @@ export default function HomeScreen() {
   const deleteTask = async (id: number) => {
     try {
       const newTasks = tasks.filter((task) => task.id !== id);
-      await AsyncStorage.setItem("my-task", JSON.stringify(newTasks));
+      await AsyncStorage.setItem('my-task', JSON.stringify(newTasks));
       setTasks(newTasks);
     } catch (error) {
       console.log(error);
@@ -65,7 +63,7 @@ export default function HomeScreen() {
         }
         return task;
       });
-      await AsyncStorage.setItem("my-task", JSON.stringify(newTasks));
+      await AsyncStorage.setItem('my-task', JSON.stringify(newTasks));
       setTasks(newTasks);
     } catch (error) {
       console.log(error);
@@ -74,31 +72,17 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={[...tasks].reverse()}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <TaskItem
-            task={item}
-            deleteTask={deleteTask}
-            handleDone={handleDone}
-          />
+      <FlatList data={[...tasks]} style={styles.flatList} keyExtractor={(item) =>
+        item.id.toString()} renderItem={({item}) => (
+          <TaskItem task={item} deleteTask={deleteTask} handleDone={handleDone}/>
         )}
       />
-      <KeyboardAvoidingView
-        style={styles.footer}
-        behavior="padding"
-        keyboardVerticalOffset={10}
-      >
-        <TextInput
-          placeholder="Adicionar nova tarefa"
-          value={taskText}
-          onChangeText={(text) => settaskText(text)}
-          style={styles.newtaskInput}
-          autoCorrect={false}
+      <KeyboardAvoidingView style={styles.footer} behavior='padding' keyboardVerticalOffset={10}>
+        <TextInput placeholder='Adicionar nova anotação' value={taskText} onChangeText={(text) => settaskText(text)}
+          style={styles.newTaskInput} autoCorrect={false}
         />
-        <TouchableOpacity style={styles.addButton} onPress={() => addtask()}>
-          <Text style={{ fontSize: 24, color: 'black' }}>A</Text>
+        <TouchableOpacity style={styles.addButton} onPress={() => addTask()}>
+          <Text style={styles.addText}>A</Text>
         </TouchableOpacity>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -106,9 +90,7 @@ export default function HomeScreen() {
 }
 
 const TaskItem = ({
-  task,
-  deleteTask,
-  handleDone,
+  task, deleteTask, handleDone,
 }: {
   task: TaskType;
   deleteTask: (id: number) => void;
@@ -117,26 +99,18 @@ const TaskItem = ({
   <View style={styles.taskContainer}>
     <View style={styles.taskInfoContainer}>
       <Checkbox
-        value={task.isDone}
-        onValueChange={() => handleDone(task.id)}
-        color={task.isDone ? "#25cc00" : undefined}
+        value={task.isDone} onValueChange={() => handleDone(task.id)} color={task.isDone ? '#25cc00' : undefined}
+        style={[styles.checkbox]}
       />
-      <Text
-        style={[
-          styles.taskText,
-          task.isDone && { textDecorationLine: "line-through" },
-        ]}
-      >
+      <Text style={[styles.taskText, task.isDone && {textDecorationLine: 'line-through'}]}>
         {task.title}
       </Text>
     </View>
-    <TouchableOpacity
-      onPress={() => {
-        deleteTask(task.id);
-        alert("Deleted " + task.id);
-      }}
-    >
-      <Text style={{ fontSize: 24, color: 'red' }}>E</Text>
+    <TouchableOpacity onPress={() => {
+      deleteTask(task.id);
+      alert('Excluido! ' + task.id);
+      }}>
+      <Text style={styles.deleteText}>E</Text>
     </TouchableOpacity>
   </View>
 );
@@ -144,45 +118,81 @@ const TaskItem = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
-    backgroundColor: "#BABABA",
+    paddingHorizontal: 16,
+    backgroundColor: '#D7FFD7',
+    fontFamily: 'Inter_400Regular, Arial, sans-serif',
+  },
+  flatList: {
+    marginTop: 15,
+    marginBottom: 40,
   },
   taskContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#FFFFFF",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
     padding: 16,
-    borderRadius: 10,
-    marginBottom: 20,
+    borderRadius: 2,
+    marginTop: 12,
   },
   taskInfoContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     gap: 10,
-    alignItems: "center",
+    alignItems: 'center',
+  },
+  checkbox: {
+    height: 24,
+    width: 24,
+    borderRadius: 2,
+    borderWidth: 2,
+    borderColor: '#333',
   },
   taskText: {
     fontSize: 16,
-    color: "#333",
-    width: "80%",
+    color: '#333',
+    width: '79%',
   },
   footer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     bottom: 20,
   },
-  newtaskInput: {
+  newTaskInput: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-    padding: 16,
-    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+    height: 50,
+    paddingLeft: 20,
+    borderRadius: 2,
+    borderWidth: 1,
     fontSize: 16,
-    color: "#333",
+    color: '#333',
   },
   addButton: {
-    backgroundColor: "#FBFF00",
-    padding: 8,
-    borderRadius: 10,
-    marginLeft: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FDFF7A',
+    height: 50,
+    width: 40,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderLeftWidth: 0,
+  },
+  addText: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#333',
+    fontSize: 24,
+  },
+  deleteText: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderColor: '#FF0000',
+    color: '#FF0000',
+    fontSize: 24,
+    height: 35,
+    width: 35,
+    borderRadius: 2,
+    borderWidth: 3,
   },
 });
